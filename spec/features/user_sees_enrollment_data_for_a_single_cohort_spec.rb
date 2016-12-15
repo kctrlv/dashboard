c95056e06c7db99dc134a4ef93d469ec6e5b53ff
David Davydov <kctrlv@gmail.com>
2016-12-14 17:42:20 -0700

    add selenium webdriver, pass tests without api calls

--- spec/features/user_sees_enrollment_data_for_a_single_cohort_spec.rb
+++ spec/features/user_sees_enrollment_data_for_a_single_cohort_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Dashboard" do
-  it "shows enrollment data for a given cohort" do
+  it "shows enrollment data for a given cohort", js: true do
     # request going to look like get to /api/v1/enrollments?megacohort=1701
     # expected json response = { fe => {enrolled: 14, maximum: 28}, be => {enrolled: 21, maximum: 28} }
     #as an authenticated user
@@ -15,6 +15,8 @@ describe "Dashboard" do
     within(".frontend-enrolled") do
       expect(page).not_to have_content('14/28 enrolled')
     end
+    #and I click the dropdown for cohorts
+    click_link "Cohorts"
     #and I click the link for cohort 1701
     click_link '1701'
     #then I see the number of seats filled in 1701 backend
